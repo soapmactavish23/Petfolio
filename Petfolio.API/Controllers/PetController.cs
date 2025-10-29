d3f6ff66538b1a53aa7d4f5e91f392e1cc21924c
henrick.nogueira <ricknogueira1231@gmail.com>
2025-10-28 21:42:16 -0300

    Classes de comunicação

---
+++ Petfolio.API/Controllers/PetController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisterPetJson), StatusCodes.Status201Created)]
+        public IActionResult Register([FromBody] RequestRegisterPetJson request)
+        {
+            return Created();
+        }
+
+    }
+}
